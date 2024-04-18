3b1e5faaf3bbf42a6c118e95f7facf4ec20d52e2
ppeixinn <leep0066@e.ntu.edu.sg>
2024-04-18 17:59:53 +0800

    staffWorker edited

--- foms/workers/StaffWorker.java
+++ foms/workers/StaffWorker.java
@@ -1,5 +1,12 @@
 package foms.workers;
 
 public class StaffWorker {
-
+    String Branch;
+    Branch branchObj = new Branch();
+    StaffWorker(String name, int age, char gender, String loginID, String branch) {
+        super(name, age, gender, loginID);
+        this.branch = branch;
+        setRole('S');
+    }
+    public void displayWorkerInformation(){}
 }
