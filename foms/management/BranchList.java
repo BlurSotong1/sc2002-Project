c0fe06cbfbb99a5eda3c948947ab1e3e0c8cd044
ChesterChiow <chesterchiow@gmail.com>
2024-04-18 17:55:22 +0800

    test

---
+++ foms/management/BranchList.java
@@ -0,0 +1,11 @@
+package foms.management;
+
+import java.util.ArrayList;
+public class BranchList {
+
+    public static ArrayList<Branch> branchList;
+
+    public BranchList (){
+        branchList = new ArrayList<Branch>();
+    }
+}
--- foms/workers/AdminWorker.java
+++ foms/workers/AdminWorker.java
@@ -1,4 +1,21 @@
 package foms.workers;
 
-public class AdminWorker {
+import java.util.ArrayList;
+import foms.management.Branch;
+import foms.management.OperationsOnBranchlist;
+
+
+public class AdminWorker extends Worker{
+    /**
+     * list of branches.
+     */
+    private ArrayList<Branch> branchList;
+
+    /**
+     * list of branches.
+     */
+    private OperationsOnBranchlist jobsOnBranchList;
+
+
+
 }
