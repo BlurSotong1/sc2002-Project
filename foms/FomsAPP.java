c51dd1fd8ff6a2f2ece9306ed4cae89ccf592d3f
choonggi <148122625+BlurSotong1@users.noreply.github.com>
2024-04-24 22:14:36 +0800

    me

--- foms/FomsAPP.java
+++ foms/FomsAPP.java
@@ -5,21 +5,28 @@ import foms.management.Branch;
 import java.io.*;
 
 public class FomsAPP implements Serializable {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) {
         Branch branch = new Branch("jp","jp", 15);
-
+    try {
         FileOutputStream fileOut = new FileOutputStream("BranchInfo.ser");
         ObjectOutputStream out = new ObjectOutputStream(fileOut);
         out.writeObject(branch);
         out.close();
         fileOut.close();
+    } catch (IOException e) {
+        System.out.println("we are screwed");
+    }
 
 
-        Branch branch2;
-        FileInputStream fileIn = new FileInputStream("foms/management/BranchInfo.ser");
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        branch2 = (Branch) in.readObject();
-        in.close();
-        fileIn.close();
+
+        try {
+            FileInputStream fileIn = new FileInputStream("foms/management/BranchInfo.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            in.close();
+            fileIn.close();
+        } catch (IOException e) {
+            System.out.println("this didnt work");
+        }
+
     }
 }
