f5f70bddc60641c243ac762332f53e1e8c221637
ChesterChiow <chesterchiow@gmail.com>
2024-04-25 15:10:34 +0800

    w

--- foms/order/PayWave.java
+++ foms/order/PayWave.java
@@ -1,7 +1,27 @@
 package foms.order;
 
 public class PayWave implements Payment{
-    public boolean processPayment(double amount){
+
+    private String name = "Paywave";
+    private boolean paymentStatus;
+
+    @Override
+    public boolean processPayment(double amount) {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    @Override
+    public void setPaymentStatus(boolean status) {
+        this.paymentStatus = status;
     }
 }
